78f03014a137ab1974bbc4f9c14912be1cb94ed5
ay <919044173@qq.com>
2022-11-11 21:58:03 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author ay.
+ * @create 2022-11-11 21:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
